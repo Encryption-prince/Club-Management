f0c6277b16fb37fc89972d297d3ab9eb4eb30857
Encryption-prince <subhammaity6827@gmail.com>
2025-03-21 14:29:22 +0530

    Initial commit

---
+++ src/main/java/com/club/management/Club/Management/Controller/QRCodeController.java
@@ -0,0 +1,13 @@
+package com.club.management.Club.Management.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payment")
+public class QRCodeController {
+
+    public String generateQR(){
+
+    }
+}
---
+++ src/test/java/com/club/management/Club/Management/ClubManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.management.Club.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
